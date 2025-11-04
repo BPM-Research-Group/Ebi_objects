cd87e2a674756da0dadf406b379fe491c4ef8d67
CI pipeline <cipipeline@example.com>
2025-11-04 12:30:20 +0100

    update to csv import parameter explanations

--- src/ebi_objects/event_log_csv.rs
+++ src/ebi_objects/event_log_csv.rs
@@ -20,7 +20,7 @@ pub const DEFAULT_QUOTE_CHARACTER: &str = "\"";
 pub const CSV_IMPORTER_PARAMETER_TRACE_ID: ImporterParameter = ImporterParameter::String {
     name: "csv_trace_id",
     short_name: "ti",
-    explanation: "The name or number of the column that contains the trace id.",
+    explanation: "The name or number of the column that contains the trace id",
     allowed_values: None,
     default_value: "0",
 };
@@ -35,7 +35,7 @@ pub const CSV_IMPORTER_PARAMETER_HAS_NO_HEADER: ImporterParameter = ImporterPara
     name: "csv_no_header",
     short_name: "nh",
     explanation: "Without this flag set, the first line is read as the header. 
-    With this flag set, the first line is read as an event. In the latter case, the columns are given numbers, starting from 0.",
+    With this flag set, the first line is read as an event. In the latter case, the columns are given numbers, starting from 0",
 };
 pub const CSV_IMPORTER_PARAMETER_SEPARATOR: ImporterParameter = ImporterParameter::String {
     name: "csv_separator",
