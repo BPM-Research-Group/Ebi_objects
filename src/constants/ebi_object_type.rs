c981136fb429cd7e421b88685c21412372c39c40
Your Name <you@example.com>
2025-11-19 14:04:16 +0100

    ord for ebiobjecttype

--- src/constants/ebi_object_type.rs
+++ src/constants/ebi_object_type.rs
@@ -54,6 +54,18 @@ impl EbiObjectType {
 
 impl Eq for EbiObjectType {}
 
+impl Ord for EbiObjectType {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.to_string().cmp(&other.to_string())
+    }
+}
+
+impl PartialOrd for EbiObjectType {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.to_string().partial_cmp(&other.to_string())
+    }
+}
+
 impl Display for EbiObjectType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
